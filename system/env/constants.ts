8ce2a27dd391901aac3d63ed97f337e33d716e36
Tomas Vitkauskas <tomas@xpmarket.com>
2023-02-21 01:15:50 +0200

    chore(app): package updates

--- system/env/constants.ts
+++ system/env/constants.ts
@@ -15,7 +15,5 @@ export const ENV: TypedEnv = {
       !!process.env.PREFETCH_ENABLED || process.env.NODE_ENV !== 'development',
     nodeEnv: process.env.NODE_ENV || 'development',
     port,
-    xummApiKey: process.env.XUMM_API_KEY,
-    xummApiSecret: process.env.XUMM_API_SECRET,
   },
 };
