899cc1e26dda72e9f8e17d723a9f4d02486729e7
Tomas Vitkauskas <tomas@xpmarket.com>
2023-03-06 16:49:08 +0100

    feat(nft-offer): dialog responsiveness

--- templates/nftinder/common/nft-create-offer-dialog/nft-offer-dialog/nft-offer-dialog-body/NftOfferDialogBodyInfo.tsx
+++ templates/nftinder/common/nft-create-offer-dialog/nft-offer-dialog/nft-offer-dialog-body/NftOfferDialogBodyInfo.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
 import { useTranslation } from 'next-i18next';
 
-import { Divider, Stack, Typography } from '@mui/material';
+import {
+  Divider,
+  Stack,
+  Theme,
+  Typography,
+  useMediaQuery,
+} from '@mui/material';
 import { ELLIPSIS_OVERFLOW } from '@xpmarket/xpm.system.theme';
+import { useThemeConfig } from '@xpmarket/xpm.system.theme-provider';
 import { Card } from '@xpmarket/xpm.ui.cards.card';
 import { AssetAvatar } from '@xpmarket/xpm.ui.gallery.asset-avatar';
 import { NumberText } from '@xpmarket/xpm.ui.number.number-text';
@@ -24,34 +31,61 @@ export const NftOfferDialogBodyInfo: FC<Props> = (props) => {
     lastPriceCurrency,
   } = props;
   const { t } = useTranslation();
+  const { applyAdaptiveBreakpoint } = useThemeConfig();
+  const isAtMobileBreakpoint = applyAdaptiveBreakpoint(
+    useMediaQuery<Theme>((theme) => theme.breakpoints.down('sm'))
+  );
 
   return (
     <Card bgVariant="secondary">
-      <Stack direction="row" spacing={1} alignItems="center">
-        <AssetAvatar
-          variant="square"
-          src={assetLogoSrc}
-          fallbackText={assetName}
-        />
-        <Stack maxWidth={280} width="100%">
-          <Typography
-            fontWeight="fontWeightBold"
-            fontSize={14}
-            sx={ELLIPSIS_OVERFLOW}
-          >
-            {assetName}
-          </Typography>
-          <Typography
-            fontWeight="fontWeightRegular"
-            fontSize={14}
-            color="secondary.main"
-            sx={ELLIPSIS_OVERFLOW}
-          >
-            {collectionName || t('common:unknownCollection')}
-          </Typography>
+      <Stack
+        direction={{
+          xs: 'column',
+          sm: 'row',
+        }}
+        spacing={1}
+        alignItems={{
+          xs: 'flex-start',
+          sm: 'center',
+        }}
+        divider={
+          <Divider
+            orientation={isAtMobileBreakpoint ? 'horizontal' : 'vertical'}
+            flexItem
+          />
+        }
+      >
+        <Stack direction="row" spacing={1} alignItems="center" width="100%">
+          <AssetAvatar
+            variant="square"
+            src={assetLogoSrc}
+            fallbackText={assetName}
+          />
+          <Stack maxWidth={280} width="100%" overflow="hidden">
+            <Typography
+              fontWeight="fontWeightBold"
+              fontSize={14}
+              sx={ELLIPSIS_OVERFLOW}
+            >
+              {assetName}
+            </Typography>
+            <Typography
+              fontWeight="fontWeightRegular"
+              fontSize={14}
+              color="secondary.main"
+              sx={ELLIPSIS_OVERFLOW}
+            >
+              {collectionName || t('common:unknownCollection')}
+            </Typography>
+          </Stack>
         </Stack>
-        <Divider orientation="vertical" flexItem />
-        <Stack maxWidth={150} width="100%">
+        <Stack
+          maxWidth={{
+            xs: '100%',
+            sm: '150px',
+          }}
+          width="100%"
+        >
           <Typography
             fontWeight="fontWeightBold"
             fontSize={14}
